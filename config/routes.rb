10e083c25499bd4f0d79c9b51210ad6cfc6ae218
Alexa <alexa.anderson@outlook.com>
2016-12-08 11:30:44 -0600

    add sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  resources :users
+  resources :users, only: [:index, :show]
+  devise_scope :users do
+    get 'signup', to: 'devise/registrations#new'
+  end
 end
