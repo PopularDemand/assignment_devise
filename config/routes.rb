99a981301dbb6c9800e953b15fd407728ecfb95d
Dexter Ford <dexter.ford.77@gmail.com>
2016-12-08 11:55:10 -0600

    Loggin in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :show]
-  devise_scope :users do
+  devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
   end
 end
